f1069e64a0949746d86ab7a99586176779227475
calvinsadewa <master23680@gmail.com>
2015-09-25 16:23:48 +0700

    first commit

---
+++ src/java/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by calvin-pc on 9/25/2015.
+ */
+public class main {
+    public static void main(String[] args) {
+        weka.gui.GUIChooser gui = new weka.gui.GUIChooser();
+        gui.main(args);
+    }
+}
